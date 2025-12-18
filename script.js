f7a8465e08eb386f5c50cb3699252d2cbfb42657
MOYORiNO <tahara.0923.tomo@gmail.com>
2025-12-18 17:29:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,6 @@ function startRealtimeListener() {
 // ===============================
 //  Firestore 保存
 // ===============================
-import { setDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 async function savePhraseToCloud(type, phrase) {
   await setDoc(doc(db, "phrases", phrase), {
